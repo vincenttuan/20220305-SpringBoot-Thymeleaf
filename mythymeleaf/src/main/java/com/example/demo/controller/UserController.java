53744338c1ed8b56cf3397c64aa25963d207ccfd
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-05 16:00:39 +0800

    update

--- mythymeleaf/src/main/java/com/example/demo/controller/UserController.java
+++ mythymeleaf/src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,17 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Random;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,6 +27,54 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute User user, Model model) {
+		model.addAttribute("_method", "POST");
+		model.addAttribute("users", userRepository.findAll());
+		return "user";
+	}
+	
+	@GetMapping("/{id}")
+	public String get(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("_method", "PUT");
+		model.addAttribute("user", userRepository.getById(id));
+		model.addAttribute("users", userRepository.findAll());
+		return "user";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		userRepository.deleteById(id);
+		return "redirect:../";
+	}
+	
+	@PostMapping("/")
+	public String add(@Valid @ModelAttribute User user, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("users", userRepository.findAll());
+			return "user";
+		}
+		userRepository.save(user);
+		return "redirect:./";
+	}
+	
+	@PutMapping("/")
+	public String update(@Valid @ModelAttribute User user, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "PUT");
+			model.addAttribute("users", userRepository.findAll());
+			return "user";
+		}
+		// 根據 JPA 規範
+		// 若有 user.id 則進行數據更新
+		userRepository.save(user);
+		return "redirect:./";
+	}
+	
+	
+	//------------------------------------------------------------------------------------
 	
 	@GetMapping("/add/sample/data")
 	@ResponseBody
